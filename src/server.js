b1d2f8302ee95746a52983e077c70c980947d3de
Lili Sazonova <lilissazonova@gmail.com>
2024-08-31 17:41:33 +0300

    add pino, cors

--- src/server.js
+++ src/server.js
@@ -1,5 +1,17 @@
 //В ньому буде знаходитись логіка роботи вашого express-серверу.
 
 import express from 'express';
+import pino from 'pino-http';
+import cors from 'cors';
+
+app.use(cors());
 
 const app = express();
+
+app.use(
+    pino({
+        transport: {
+            target: 'pino-pretty',
+        },
+    }),
+);
