c5f7b642ed9e0d7c1c94b4fec961c3f50c98f644
Lili Sazonova <lilissazonova@gmail.com>
2024-09-02 13:35:30 +0300

    add mongoose, env, initMongo

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
+import { initMongoCollection } from './db/initMongoCollection.js';
 import { startServer } from './server.js';
 
-startServer();
+const bootstrap = async () => {
+    await initMongoCollection();
+    startServer();
+};
+
+bootstrap();
