00162012e3200edd1d0893040383aefac73bd155
Lili Sazonova <lilissazonova@gmail.com>
2024-09-04 19:14:45 +0300

    add route POST

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -1,4 +1,4 @@
-import { getAllContacts, getContactById } from "../services/contacts.js";
+import { createContact, getAllContacts, getContactById } from "../services/contacts.js";
 import createHttpError from 'http-errors';
 export const getContactsController = async (req, res, next) => {
     try {
@@ -27,4 +27,24 @@ export const getContactByIdController = async (req, res, next) => {
         message: `Sucessfully found contact with id ${contactId}!`,
         data: contact,
     });
+};
+export const createContactController = async (req, res, next) => {
+    try {
+        const { name, phoneNumber, email, isFavourite, contactType } = req.body;
+
+        if (!name || !phoneNumber || !contactType) {
+            throw createHttpError(400, "Missing required fields: name, phoneNumber, and contactType are required");
+        }
+
+        const contact = await createContact({ name, phoneNumber, email, isFavourite, contactType });
+
+        res.status(201).json({
+            status: 201,
+            message: `Successfully created a contact!`,
+            data: contact,
+        });
+    } catch (err) {
+        next(err);
+    }
+
 };
--- src/server.js
+++ src/server.js
@@ -13,6 +13,7 @@ export const startServer = () => {
     const app = express();
 
     app.use(express.json());
+
     app.use(cors());
 
     app.use(
