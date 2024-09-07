9b57f764c466a1a50b96e590f56474103c00eb96
Lili Sazonova <lilissazonova@gmail.com>
2024-09-07 18:58:32 +0300

    bug fix

--- src/validation/contacts.js
+++ src/validation/contacts.js
@@ -2,9 +2,9 @@ import Joi from 'joi';
 
 export const createContactSchema = Joi.object({
     name: Joi.string().min(3).max(20).required(),
-    phoneNumber: Joi.number().required(),
+    phoneNumber: Joi.string().pattern(/^\+380\d{9}$/).required(),
     email: Joi.string().email().min(3).max(20).required(),
-    isFavorite: Joi.boolean(),
+    isFavourite: Joi.boolean(),
     contactType: Joi.string()
         .min(3)
         .max(20)
@@ -14,9 +14,9 @@ export const createContactSchema = Joi.object({
 
 export const updateContactSchema = Joi.object({
     name: Joi.string().min(3).max(20),
-    phoneNumber: Joi.number(),
+    phoneNumber: Joi.string().pattern(/^\+380\d{9}$/),
     email: Joi.string().email().min(3).max(20),
-    isFavorite: Joi.boolean(),
+    isFavourite: Joi.boolean(),
     contactType: Joi.string()
         .min(3)
         .max(20)
