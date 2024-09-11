520c4cffbe49f4e2dce88592385861a156d47246
Lili Sazonova <lilissazonova@gmail.com>
2024-09-11 18:55:15 +0300

    add auth/refresh router

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,5 +1,5 @@
 import { THIRTY_DAYS } from "../constants/index.js";
-import { loginUser, registerUser } from "../services/auth.js";
+import { loginUser, refreshUserSession, registerUser } from "../services/auth.js";
 
 export const registerUserController = async (req, res, next) => {
     const user = await registerUser(req.body);
@@ -26,6 +26,34 @@ export const loginUserController = async (req, res) => {
     res.json({
         status: 200,
         message: `Successfully logged in an user!`,
+        data: {
+            accessToken: session.accessToken,
+        },
+    });
+};
+
+const setupSession = (res, session) => {
+    res.cookie('refreshToken', session.refreshToken, {
+        httpOnly: true,
+        expires: new Date(Date.now() + THIRTY_DAYS),
+    });
+    res.cookie('sessionId', session._id, {
+        httpOnly: true,
+        expires: new Date(Date.now() + THIRTY_DAYS),
+    });
+};
+
+export const refreshUserSessionController = async (req, res) => {
+    const session = await refreshUserSession({
+        sessionId: req.cookies.sessionId,
+        refreshToken: req.cookies.refreshToken,
+    });
+
+    setupSession(res, session);
+
+    res.json({
+        status: 200,
+        message: `Successfully refreshed a session!`,
         data: {
             accessToken: session.accessToken,
         },
