fc6c255ad0341b2099b78586b50251b49c5d77d7
Lili Sazonova <lilissazonova@gmail.com>
2024-09-21 17:09:28 +0300

    add rout /api-docs

--- src/server.js
+++ src/server.js
@@ -8,6 +8,7 @@ import { notFoundHandler } from './middlewares/notFoundHandler.js';
 import router from './routers/index.js';
 import cookieParser from 'cookie-parser';
 import { UPLOAD_DIR } from './constants/index.js';
+import { swaggerDocs } from './middlewares/swaggerDocs.js';
 
 const PORT = Number(env('PORT', '3000'));
 
@@ -31,6 +32,8 @@ export const startServer = () => {
     app.use(router);
 
     app.use('/uploads', express.static(UPLOAD_DIR));
+
+    app.use('/api-docs', swaggerDocs());
 
     app.use('*', notFoundHandler);
 
